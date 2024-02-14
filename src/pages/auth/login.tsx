a52a71db48de2e2caf303688cf9ebad44b04bded
Muhless <nuryadi.muhta@gmail.com>
2024-02-14 16:17:29 +0700

    eps 4: styling

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,17 +1,10 @@
-import Link from "next/link"
-import { useRouter } from "next/router"
+import LoginViews from "@/views/Auth/Login/login"
 
 const LoginPage = () => {
-    const { push } = useRouter();
-    const handlerLogin = () => {
-        push('/product')
-    }
     return (
-        <div>
-            <h1>Login</h1>
-            <button onClick={handlerLogin}>Login</button>
-            <p>Belum punya akun ? Registrasi <Link href={'/auth/register'}>disini</Link></p>
-        </div>
+        <>
+            <LoginViews />
+        </>
     )
 }
 
